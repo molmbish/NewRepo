2225ff3600314445ff719d376ee89302ff49896c
Molli Bishop <molmbish@ututm.onmicrosoft.com>
2020-08-29 11:04:06 -0500

    Hello

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-﻿//Name: Molli Bishop
-using System;
-
-namespace ConsoleApp2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
